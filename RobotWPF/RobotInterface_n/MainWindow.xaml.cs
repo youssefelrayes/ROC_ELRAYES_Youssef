16ce7e605b72afedae250e33e53801ba8c7ba9cf
HANNA Bruno <brncelee@proton.me>
2023-03-31 16:28:03 +0200

    Liaison série hexadécimale

--- RobotWPF/RobotInterface_n/MainWindow.xaml.cs
+++ RobotWPF/RobotInterface_n/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using ExtendedSerialPort;
+using RobotInterface_n;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,44 +28,59 @@ namespace RobotInterface
     {
         ReliableSerialPort serialPort1;
         DispatcherTimer timerAffichage;
+        Robot robot = new Robot();
 
+        string receivedText;
 
 
         public MainWindow()
         {
             InitializeComponent();
 
+            serialPort1 = new ReliableSerialPort("COM8", 115200, System.IO.Ports.Parity.None, 8, System.IO.Ports.StopBits.One);
+            serialPort1.OnDataReceivedEvent += SerialPort1_OnDataReceivedEvent;
+            serialPort1.Open();
+
             timerAffichage = new DispatcherTimer();
             timerAffichage.Interval = new TimeSpan(0, 0, 0, 0, 100);
-            timerAffichage.Tick += TimerAffichage_Tick; ;
+            timerAffichage.Tick += TimerAffichage_Tick;
             timerAffichage.Start();
-
-            serialPort1 = new ReliableSerialPort("COM8", 115200, System.IO.Ports.Parity.None, 8, System.IO.Ports.StopBits.One);
-            serialPort1.DataReceived += SerialPort1_DataReceived;
-            serialPort1.Open();
         }
 
+
+
+   
+        private void SerialPort1_OnDataReceivedEvent(object sender, DataReceivedArgs e)
+        {
+
+            receivedText += Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
+            for(int i = 0 ; i<e.Data.Length ; i++ )
+            {
+                byte b = e.Data[i];   
+                robot.byteListReceived.Enqueue(b);
+            }    
+        }
 
         private void TimerAffichage_Tick(object sender, EventArgs e)
         {
-          
+            /*
+            if(receivedText != "")
+            { 
+                textBoxReception.Text += receivedText;
+                receivedText = "";
+
+             }  
+            */
+
+            while(robot.byteListReceived.Count > 0)
+            {
+                textBoxReception.Text += robot.byteListReceived.Dequeue().ToString();
+            }
         }
 
-        private void SerialPort1_DataReceived(object sender, DataReceivedArgs e)
-        {
-            textBoxReception.Text += Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
-        }
-
-
-        private void buttonEnvoyer_Click(object sender, RoutedEventArgs e)
-        {
-            ToggleColor();
-            SendMessage();
-        }
 
         void SendMessage()
         {
-            //textBoxReception.Text += "Reçu : Message : " + textBoxEmission.Text + "\n";
             serialPort1.WriteLine(textBoxEmission.Text);
             textBoxEmission.Clear();
         }
@@ -72,9 +88,32 @@ namespace RobotInterface
         void ToggleColor()
         {
             if (buttonEnvoyer.Background == Brushes.RoyalBlue)
-            { buttonEnvoyer.Background = Brushes.Aqua; }
+            {
+                buttonEnvoyer.Background = Brushes.Aqua; 
+            }
             else
-            { buttonEnvoyer.Background = Brushes.RoyalBlue; }
+            { 
+                buttonEnvoyer.Background = Brushes.RoyalBlue;
+            }
+        }
+
+        private void buttonEnvoyer_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleColor();
+            SendMessage();
+        }
+
+
+        byte[] byteList = new byte[20];
+        private void Test_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                byteList[i] = (byte)(2 * i);
+            }
+            serialPort1.Write(byteList, 0, byteList.Length);
+            
+
         }
     }
 
---
+++ RobotWPF/RobotInterface_n/Robot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotInterface_n
+{
+    public class Robot
+    {
+        public string receivedText = "";
+        public float distanceTelemetreDroit;
+        public float distanceTelemetreCentre;
+        public float distanceTelemetreGauche;
+        public Queue<byte> byteListReceived;
+
+
+        public Robot()
+        {
+          
+            byteListReceived = new Queue<byte>();
+
+        }
+    }
+}
