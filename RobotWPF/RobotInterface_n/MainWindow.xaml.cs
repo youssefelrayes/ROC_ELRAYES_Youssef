72084b6e1b38c367a8f4c6dc971108ab54d81149
HANNA Bruno <brncelee@proton.me>
2023-04-11 16:28:16 +0200

    Encodage et Décodage des messages (Fini)
    11/4/2023

--- RobotWPF/RobotInterface_n/MainWindow.xaml.cs
+++ RobotWPF/RobotInterface_n/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using RobotInterface_n;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -74,7 +75,8 @@ namespace RobotInterface
 
             while(robot.byteListReceived.Count > 0)
             {
-                textBoxReception.Text += robot.byteListReceived.Dequeue().ToString();
+                DecodeMessage(robot.byteListReceived.Dequeue());
+                //textBoxReception.Text += robot.byteListReceived.Dequeue().ToString();
             }
         }
 
@@ -105,6 +107,7 @@ namespace RobotInterface
 
 
         byte[] byteList = new byte[20];
+
         private void Test_Click(object sender, RoutedEventArgs e)
         {
             /*
@@ -115,28 +118,119 @@ namespace RobotInterface
             */
 
             int f = 0x0080;
-            byte[] payload = new byte[20];
+            byte[] payload = Encoding.ASCII.GetBytes("Bonjour");
             int payload_length = payload.Length;
             UartEncodeAndSendMessage(f, payload_length, payload);
 
+            
             textBoxEmission.Clear();
         }
         
         byte CalculateChecksum(int msgFunction,int msgPayloadLength, byte[] msgPayload)
         {
-
-            string Sum = "FE" + msgFunction.ToString("X") + msgPayloadLength.ToString("X") + BitConverter.ToString(msgPayload);
-            string Sub = Sum.Substring("FF".Length);
-            byte Checksum = Convert.ToByte(Sub);
-            return Checksum;
-        }
-
-        void UartEncodeAndSendMessage(int msgFunction, int msgPayloadLength, byte[] msgPayload)
-        {
-            byte CheckSum = CalculateChecksum(msgFunction, msgPayloadLength, msgPayload);
-            serialPort1.WriteLine("FE" + msgFunction.ToString() + msgPayloadLength.ToString() + BitConverter.ToString(msgPayload) + Convert.ToString(CheckSum));
+            byte CheckSum = 0;
+            CheckSum ^= (byte) (0xFE >> 0);
+            CheckSum ^= (byte)(msgFunction >> 8);
+            CheckSum ^= (byte)(msgFunction >> 0);
+            CheckSum ^= (byte)(msgPayloadLength >> 0);
+            for (int i = 0; i < msgPayloadLength; i++)
+            {
+                CheckSum ^= msgPayload[i];
+            }
+            //textBoxReception.Text = CheckSum.ToString("X");
+            return CheckSum;
         }
         
+        void UartEncodeAndSendMessage(int msgFunction, int msgPayloadLength, byte[] msgPayload)
+        {
+            var message = new byte[msgPayloadLength + 6];
+            int pos = 0;
+            message[pos++] = 0xFE;
+            message[pos++] = (byte)(msgFunction >> 8);
+            message[pos++] = (byte)(msgFunction >> 0);
+            message[pos++] = (byte)(msgPayloadLength >> 8);
+            message[pos++] = (byte)(msgPayloadLength >> 0);
+            for (int i = 0 ; i<msgPayloadLength ; i++)
+            {
+                message[pos++] = msgPayload[i];
+            }
+            message[pos++] = CalculateChecksum(msgFunction, msgPayloadLength, msgPayload);
+
+            serialPort1.Write(message, 0, message.Length);
+
+        }
+        
+        public enum StateReception
+        {
+            Waiting,
+            FunctionMSB,
+            FunctionLSB,
+            PayloadLengthMSB,
+            PayloadLengthLSB,
+            Payload,
+            CheckSum
+        }
+
+        StateReception rcvState = StateReception.Waiting;
+
+        int msgDecodedFunction = 0;
+        int msgDecodedPayloadLength = 0;
+        byte[] msgDecodedPayload;
+        int msgDecodedPayloadIndex = 0;
+
+        private void DecodeMessage(byte c)
+        {
+            switch (rcvState)
+            {
+                case StateReception.Waiting:
+                    if (c == 0xFE)
+                        rcvState = StateReception.FunctionMSB;
+                    break;
+                case StateReception.FunctionMSB:
+                    msgDecodedFunction = c << 8;
+                    rcvState = StateReception.FunctionLSB;
+                    break;
+                case StateReception.FunctionLSB:
+                    msgDecodedFunction += c << 0;
+                    rcvState = StateReception.PayloadLengthMSB;
+                    break;
+                case StateReception.PayloadLengthMSB:
+                    msgDecodedPayloadLength = c << 8;
+                    rcvState = StateReception.PayloadLengthLSB;
+                    break;
+                case StateReception.PayloadLengthLSB:
+                    msgDecodedPayloadLength += c << 0;
+                    if (msgDecodedPayloadLength == 0)
+                        rcvState = StateReception.CheckSum;
+                    else
+                    {
+                        rcvState = StateReception.Payload;
+                        msgDecodedPayloadIndex = 0;
+                        msgDecodedPayload = new byte[msgDecodedPayloadLength];
+                    }
+                    break;
+                case StateReception.Payload:
+                    msgDecodedPayload[msgDecodedPayloadIndex++] = c ;
+                    if (msgDecodedPayloadIndex >= msgDecodedPayloadLength)
+                    {
+                        rcvState = StateReception.CheckSum;
+                    }
+                    break;
+                case StateReception.CheckSum:                    
+                    byte receivedChecksum = c;
+                    byte calculatedChecksum = CalculateChecksum(msgDecodedFunction, msgDecodedPayloadLength, msgDecodedPayload);
+                if (calculatedChecksum == receivedChecksum)
+                    {
+                      //Success, on a un message valide
+                    }
+                    rcvState = StateReception.Waiting;
+                    break;
+                default:
+                    rcvState = StateReception.Waiting;
+                    break;
+            }
+        }
+
 
 
 
